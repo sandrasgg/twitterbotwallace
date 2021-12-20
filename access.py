7ab14f0172283b6810ff46f9753b196b8430fb89
Sandra Serrano <sandraserrg@gmail.com>
2021-12-20 23:22:23 +0100

    Remove comments

--- access.py
+++ access.py
@@ -1,15 +1,9 @@
-# Credentials for the Twitter bot account
 import tweepy
 import os
 from os import environ
 
-# Consumer:
 CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
 CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
 
-# Access:
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
-
-
-
